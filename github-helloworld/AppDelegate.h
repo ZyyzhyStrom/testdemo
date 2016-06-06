774035ddc8e28235b48bd639e44d0f9f63093cb0
阿杜 <your@email.com>
2016-06-06 16:33:11 +0800

    Initial Commit

---
+++ github-helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github-helloworld
+//
+//  Created by 阿杜 on 16/6/6.
+//  Copyright © 2016年 timepost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github-helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github-helloworld
+//
+//  Created by 阿杜 on 16/6/6.
+//  Copyright © 2016年 timepost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
